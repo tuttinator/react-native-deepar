27bd750530c5df501954dcde2911b73263084d3c
Caleb Tutty <github@calebtutty.com>
2023-04-14 09:42:06 +0700

    fix: remove conditional import
    As per other examples, such as https://github.com/expo/expo/pull/15626/files
    this is required for Expo

--- ios/RNTCameraModule.h
+++ ios/RNTCameraModule.h
@@ -5,16 +5,7 @@
 //  Created by Work on 5.07.2022.
 //
 
-#ifndef RNTCameraModule_h
-#define RNTCameraModule_h
-
-#if __has_include("RCTBridgeModule.h")
-#import "RCTBridgeModule.h"
-#else
 #import <React/RCTBridgeModule.h>
-#endif
 
 @interface RNTCameraModule : NSObject <RCTBridgeModule>
 @end
-
-#endif /* RNTCameraModule_h */
